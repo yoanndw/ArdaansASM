c0a84760990b62f81954549a3497900d727362eb
yoanndw <yoanndewilde@gmail.com>
2020-12-10 08:19:37 +0100

    [Parser refactor] Generate instruction complete opcode

--- Ardaans/Assembly/CodeGenerator.cs
+++ Ardaans/Assembly/CodeGenerator.cs
@@ -11,6 +11,39 @@ namespace Ardaans.Assembly
 {
     public class CodeGenerator
     {
-        
+        private static InstructionNode1Op[] possiblePatterns =
+        {
+
+        };
+
+        private List<InstructionNode1Op> ast;
+        private byte[] output;
+
+        public CodeGenerator(Input input, List<InstructionNode1Op> ast)
+        {
+            this.ast = ast;
+        }
+
+        private byte[] GenerateInstructionCode(InstructionNode1Op instruction)
+        {
+            // Get instruction opcode
+            int opcode = Array.FindIndex
+            (
+                possiblePatterns,
+                pat => instruction.HasSamePattern(pat)
+            );
+
+            if (opcode != -1) // if pattern exists
+            {
+                byte bOpcode = (byte)opcode;
+
+                List<byte> code = instruction.GenerateOperandOpcode().ToList(); // { op1, op2 }
+                code.Insert(0, bOpcode); // { instruction, op1, op2 }
+
+                return code.ToArray();
+            }
+
+            return null;
+        }
     }
 }
